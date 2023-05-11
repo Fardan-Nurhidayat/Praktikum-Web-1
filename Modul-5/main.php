7508742fa1f5cd33f169cfb61b35fc6e4dc315ba
Fardan Nurhidayat <129834353+Fardan-Nurhidayat@users.noreply.github.com>
2023-05-11 21:04:41 +0700

    Chapter-5

---
+++ Modul-5/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contoh Kode PHP di HTML</title>
+</head>
+<body>
+	<h1>Halo, dunia!</h1>
+
+	<?php
+	// Ini adalah kode PHP
+	$nama = "John";
+	$umur = 25;
+	echo "<p>Nama saya adalah " . $nama . " dan saya berumur " . $umur . " tahun.</p>";
+	?>
+	
+</body>
+</html>
