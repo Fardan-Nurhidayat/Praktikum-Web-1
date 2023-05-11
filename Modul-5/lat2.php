ae573556fc3d7c4ca3d47200335f029f6dd0740c
Fardan Nurhidayat <129834353+Fardan-Nurhidayat@users.noreply.github.com>
2023-05-11 21:08:23 +0700

    Delete lat2.php

--- Modul-5/lat2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nim = "0411500400";
- $nama = 'Chotimatul Musyarofah';
-  $alamat = "Tasikmalaya , Jawa Barat";
- echo "NIM : " . $nim . "<br>";
- echo "Nama : $nama <br>"; 
- echo "Alamat : $alamat";
-?>
